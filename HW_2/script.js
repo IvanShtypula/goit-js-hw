4eef966cd06ed9eb7d3d2a55ad2a2f7c6242b85c
Mypaxa4e <ivanshtypula@gmail.com>
2020-05-15 15:14:52 +0300

    all off

--- HW_2/script.js
+++ HW_2/script.js
@@ -3,5 +3,5 @@
 // import './tasks/task-3.js';
 // import './tasks/task-4.js';
 // import './tasks/task-5.js';
-import './tasks/task-6.js';
+// import './tasks/task-6.js';
 // import './tasks/task-7.js';
