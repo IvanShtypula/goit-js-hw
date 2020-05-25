3df26287af32e375020287852ecc19046fd3e67f
Mypaxa4e <ivanshtypula@gmail.com>
2020-05-25 15:56:51 +0300

    HW-4

---
+++ HW_4/script.js
@@ -0,0 +1,4 @@
+// import './tasks/task-1.js';
+import './tasks/task-2.js';
+
+
---
+++ HW_4/tasks/test.js
@@ -0,0 +1,12 @@
+const hotel = {
+  name: 'Resort Hotel',
+  stars: 5,
+  capacity: 100,
+};
+
+hotel.greet = function() {
+  console.log('Welcome!');
+};
+
+hotel.greet(); // Welcome
+console.log(hotel);
